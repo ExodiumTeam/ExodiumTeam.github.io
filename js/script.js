34e7531486829747d94186f05feedca6b9f4b694
UserDev <exodiumt@gmail.com>
2023-10-17 01:36:50 -0300

    add skills section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const sidebar = document.querySelector('.sidebar')
 const btnMenu = document.querySelector('.button-menu')
 const links = document.querySelectorAll('.sidebar div ul li a')
+const section = document.querySelector('#skills');
 
 btnMenu.addEventListener('click', () => {
 	btnMenu.classList.toggle('active')
@@ -14,3 +15,24 @@ links.forEach(link => {
 		})
 	})
 })
+
+function handleAnimation(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.querySelectorAll('.skill-bar .bar span').forEach((bar) => {
+        bar.classList.add('animate');
+      })
+    } else {
+    	entry.target.querySelectorAll('.skill-bar .bar span').forEach((bar) => {
+        bar.classList.remove('animate');
+      })
+    }
+  })
+}
+
+const observer = new IntersectionObserver(handleAnimation, {
+  root: null,
+  threshold: 0.03,
+})
+
+observer.observe(section)
